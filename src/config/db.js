0af4f9eb0b5c9376170dadb48531be492e690f52
MThanh <doanminhthanh0302@gmail.com>
2025-11-04 18:09:50 +0700

    Update db.js for Render env

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,12 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env' });
-
-// Kết nối bằng DATABASE_URL do Render cấp
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: {
       require: true, // bắt buộc với Render
-      rejectUnauthorized: false, // bỏ verify chứng chỉ
+      rejectUnauthorized: false,
     },
   },
   pool: {
